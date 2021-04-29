3ca1b9a6f6d45ed45e6011fd6bfe3bfa2e2cac24
admin <admin@DESKTOP-VFVIQ95>
2021-04-29 18:02:05 +0530

    insertion and deletion

--- src/data/structures/linked/list/SingleLinkedList.java
+++ src/data/structures/linked/list/SingleLinkedList.java
@@ -1,55 +1,89 @@
 package data.structures.linked.list;
 
 public class SingleLinkedList {
-	Node head; // head of the list
-
-	/* Linked list Node */
+	public Node head = null; // head of the list
+	public Node tail = null;
 
 	/*
 	 * Linked list Node. This inner class is made static so that main() can access
 	 * it
 	 */
-
 	static class Node {
-		public int data;
-		public Node next;
+		int data;
+		Node next;
 
 		/*
 		 * Constructor to create a new node Next is by default initialized as null
 		 */
-		public Node(int d) {
-			data = d;
-			next = null;
+		public Node(int data) {
+			this.data = data;
+			this.next = null;
+		}
+	}
+
+	private void addNode(int data) {
+		Node newNode = new Node(data);
+		if (head == null) {
+			head = newNode;
+			tail = newNode;
+		} else {
+			tail.next = newNode;
+			tail = newNode;
+		}
+	}
+
+	private void display() {
+		Node current = head;
+		if (head == null) {
+			System.out.println("list is empty");
+			return;
+		}
+		System.out.println("nodes available in the single linked list");
+		while (current != null) {
+			System.out.println(current.data + " ");
+			current = current.next;
+		}
+		System.out.println();
+	}
+
+	 void deleteNodeByKey(int key) {
+		Node currentNode = head;
+		Node previous = null;
+		// case 1 : if the key is the first element
+		if (currentNode != null && currentNode.data == key) {
+			head = currentNode.next;
+			System.out.println(key + " found and deleted");
+			return;
+		}
+		// case 2 : if the key is not in the middle of head and tail
+		while (currentNode != null && currentNode.data != key) {
+			previous = currentNode;
+			currentNode = currentNode.next;
+		}
+		// delete the matching node
+		if (currentNode != null) {
+			previous.next = currentNode.next;
+			System.out.println(key + " found and deleted");
+		}
+		// case 3: the key is not present
+		if (currentNode == null) {
+			System.out.println(key + " not found");
 		}
 	}
 
 	public static void main(String[] args) {
-		
+
 		SingleLinkedList sll = new SingleLinkedList();
-		//head and node creation 
-		sll.head = new Node(1);
-		Node second = new Node(20);
-		Node third = new Node(3);
-		Node fourth = new Node(4);
-		Node fifth = new Node(44);
-		
-		//linked list allignment 
-		
-		sll.head.next = second;
-		second.next = third;
-		third.next = fourth;
-		fourth.next = fifth;
-		sll.printList();
+		sll.addNode(1);
+		sll.addNode(2);
+		sll.addNode(54);
+		sll.addNode(98);
+		sll.addNode(0);
+		sll.addNode(0);
+		sll.addNode(4);
+		sll.display();
+		sll.deleteNodeByKey(0);
+		sll.deleteNodeByKey(0);
+		sll.display();
 	}
-
-	private void printList() {
-		// TODO Auto-generated method stub
-		Node n = head;
-		while (n != null) {
-			System.out.println(n.data);
-			n = n.next;
-		}
-	}
-
-
 }
